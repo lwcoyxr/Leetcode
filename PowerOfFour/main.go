2f0446012bda2da1a3708d06f7fa7031f756dccf
Stanislav Sliesariev <stanislav.sliesariev@reface.ai>
2024-03-16 11:47:31 +0200

    power of four

---
+++ PowerOfFour/main.go
@@ -0,0 +1,3 @@
+func powerOfFour(n int) bool {
+	return n > 0 && (n&(n-1) == 0) && (n&0x55555555 != 0) //same power check of two but with additional mask check for making sure that every 1st, 5th, 9th one position of pow 4 is as expected
+}
